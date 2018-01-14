6d54b12148d3103737d552a22b59e9cc029f7c52
blueappdev <appdev@bluewin.ch>
2018-01-14 12:50:06 +0100

    converted to a class

--- mp3towav.py
+++ mp3towav.py
@@ -9,34 +9,35 @@
 
 import sys, getopt, glob, os.path
 
-def process(mp3Filename):
-    print "Process", mp3Filename
-    baseFilename, extension = os.path.splitext(mp3Filename)
-    assert extension.lower() == ".mp3", "only mp3 files are supported"
-    wavFilename = baseFilename + ".wav"
-    cmd = "ffmpeg -y -v 0 -i \"%s\" \"%s\"" % (mp3Filename, wavFilename)
-    code = os.system(cmd)
-    if code != 0:
-         print "ffmpeg failed"
-         sys.exit(2)
+class MP3ToWavConverter:
+    def __init__(self, arguments):
+        print "MP3 to WAV converter using ffmpeg"
+        self.counter = 0
+        opts, args = getopt.getopt(arguments,"h")
+        if opts != [] or args == []:
+            self.usage()
+            sys.exit(2)            
+        for each in args:
+            for filename in glob.glob(each):
+                self.processFile(filename)
+        print self.counter, "mp3 files processed"
 
-def usage():
-    print "Usage:"
-    print "       mp3towav file1.mp3 file2.mp3 file3.mp3"
-    print "       mp3towav *.mp3"
+    def processFile(self, mp3Filename):
+        print "Process", mp3Filename
+        baseFilename, extension = os.path.splitext(mp3Filename)
+        assert extension.lower() == ".mp3", "only mp3 files are supported"
+        wavFilename = baseFilename + ".wav"
+        cmd = "ffmpeg -y -v 0 -i \"%s\" \"%s\"" % (mp3Filename, wavFilename)
+        code = os.system(cmd)
+        if code != 0:
+             print "ffmpeg failed"
+             sys.exit(2)
+        self.counter += 1
+
+    def usage(self):
+        print "Usage:"
+        print "       mp3towav file1.mp3 file2.mp3 file3.mp3"
+        print "       mp3towav *.mp3"
 
 if __name__ == "__main__":
-    print "MP3 to WAV converter using ffmpeg"
-
-    opts, args = getopt.getopt(sys.argv[1:],"h")
-
-    if opts != [] or args == []:
-        usage()
-        sys.exit(2)
-        
-    counter = 0
-    for each in args:
-        for filename in glob.glob(each):
-            counter = counter + 1
-            process(filename)
-    print counter, "mp3 files processed"
+    MP3ToWavConverter(sys.argv[1:])
